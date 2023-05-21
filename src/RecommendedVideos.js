f380fefbc9010049e9a0c24c90e497cc632d415a
TakehiroTsukagoshi <takehiro.thanks@gmail.com>
2023-05-21 11:32:23 +0900

    レコメンド表示

---
+++ src/RecommendedVideos.js
@@ -0,0 +1,63 @@
+import React from 'react'
+import "./RecommendedVideos.css"
+import VideoCard from './VideoCard'
+
+function RecommendedVideos() {
+  return (
+    <div className="recommendedVideos">
+      <h2>Recommended</h2>
+      <div className="recommendedVideos__videos">
+        <VideoCard 
+          title="I challenge build the Youtube clone with React"
+          views="2.3M Views"
+          timestamp="3 days ago"
+          channel="Takehiro"
+          channelImage="https://icons.iconarchive.com/icons/rokey/popo-emotions/128/matrix-icon.png"
+          image="https://i.pinimg.com/originals/7f/8e/e8/7f8ee8380d3b78c41560d77013cc6394.png"
+        />
+        <VideoCard 
+          title="I challenge build the Youtube clone with React"
+          views="2.3M Views"
+          timestamp="3 days ago"
+          channel="Takehiro"
+          channelImage="https://icons.iconarchive.com/icons/rokey/popo-emotions/128/matrix-icon.png"
+          image="https://i.pinimg.com/originals/7f/8e/e8/7f8ee8380d3b78c41560d77013cc6394.png"
+        />
+        <VideoCard 
+          title="I challenge build the Youtube clone with React"
+          views="2.3M Views"
+          timestamp="3 days ago"
+          channel="Takehiro"
+          channelImage="https://icons.iconarchive.com/icons/rokey/popo-emotions/128/matrix-icon.png"
+          image="https://i.pinimg.com/originals/7f/8e/e8/7f8ee8380d3b78c41560d77013cc6394.png"
+        />
+        <VideoCard 
+          title="I challenge build the Youtube clone with React"
+          views="2.3M Views"
+          timestamp="3 days ago"
+          channel="Takehiro"
+          channelImage="https://icons.iconarchive.com/icons/rokey/popo-emotions/128/matrix-icon.png"
+          image="https://i.pinimg.com/originals/7f/8e/e8/7f8ee8380d3b78c41560d77013cc6394.png"
+        />
+        <VideoCard 
+          title="I challenge build the Youtube clone with React"
+          views="2.3M Views"
+          timestamp="3 days ago"
+          channel="Takehiro"
+          channelImage="https://icons.iconarchive.com/icons/rokey/popo-emotions/128/matrix-icon.png"
+          image="https://i.pinimg.com/originals/7f/8e/e8/7f8ee8380d3b78c41560d77013cc6394.png"
+        />
+        <VideoCard 
+          title="I challenge build the Youtube clone with React"
+          views="2.3M Views"
+          timestamp="3 days ago"
+          channel="Takehiro"
+          channelImage="https://icons.iconarchive.com/icons/rokey/popo-emotions/128/matrix-icon.png"
+          image="https://i.pinimg.com/originals/7f/8e/e8/7f8ee8380d3b78c41560d77013cc6394.png"
+        />
+      </div>
+    </div>
+  )
+}
+
+export default RecommendedVideos
